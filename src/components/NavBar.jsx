c85d24bbfede4529c6fa869f5b09a2175beecd02
Dev Mamgain <112395945+devmamgain@users.noreply.github.com>
2024-07-26 21:57:54 +0530

    final commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 const NavBar = ({ setSearching }) => {
   return (
-    <nav className="flex sm2:p-3 sm:py-3 sm:px-1 bg-blue-500 fixed z-20 top-0 w-[100%]">
-      <div className="flex gap-2 sm2:ml-[5%] ">
+    <nav className="flex sm2:p-3 sm:py-3 sm:px-1 bg-blue-500 fixed z-20 top-0 w-[100%] justify-center h-16 sm:gap-2 lg:gap-0">
+      <div className="lg:absolute left-0 flex gap-2 sm2:ml-[5%] sm:mt-1 sm2:mt-0">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.7" stroke="currentColor" className="sm2:size-9 sm:size-7 text-white">
   <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
 </svg>
@@ -13,7 +13,7 @@ const NavBar = ({ setSearching }) => {
       <input
         type="text"
         placeholder="Search by Title..."
-        className=" rounded-3xl sm2:px-4 sm:px-2 focus:outline-none mx-auto"
+        className=" rounded-3xl sm2:px-4 sm:px-2 focus:outline-none "
         onChange={(e) =>setTimeout(()=> setSearching(e.target.value),[500])}
       />
     </nav>
